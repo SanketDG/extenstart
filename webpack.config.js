3b347f59646edf8d3e9c13127dcfb472cfd72b5c
Sanket Dasgupta <sanketdasgupta@gmail.com>
2020-06-06 05:49:08 +0530

    fix: live reload with background script inclusion

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,44 @@
-const path = require("path");
-const ExtensionReloader = require("webpack-extension-reloader");
+const { resolve } = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ExtensionReloaderPlugin = require("webpack-extension-reloader");
 
 const mode = process.env.NODE_ENV;
 module.exports = {
   mode,
   devtool: "inline-source-map",
-  watch: true,
   entry: {
     "content-script": "./src/content-script.js",
+    "background": "./src/background.js"
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
+    publicPath: ".",
+    path: resolve(__dirname, "dist/"),
     filename: "[name].bundle.js",
+    libraryTarget: "umd",
   },
   plugins: [
-    new ExtensionReloader(),
+    /***********************************************************************/
+    /* By default the plugin will work only when NODE_ENV is "development" */
+    /***********************************************************************/
+    new ExtensionReloaderPlugin({
+      entries: {
+        contentScript: "content-script",
+        background: "background",
+        // extensionPage: "popup",
+      },
+      // Also possible to use
+      // manifest: resolve(__dirname, "manifest.json")
+    }),
+
     new CopyWebpackPlugin({
       patterns: [
-        { from: 'manifest.json' },
-      ]
+        { from: 'manifest.json'},
+      ],
     }),
   ],
+  module: {
+    rules: [
+
+    ],
+  },
 };
